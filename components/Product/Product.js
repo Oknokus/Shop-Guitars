16ec3deb59851382b29c1be10fa60995e2228dbd
Oknokus <oknokus@gmail.com>
2023-03-10 01:10:53 +0300

    create & styling component Product

---
+++ components/Product/Product.js
@@ -0,0 +1,27 @@
+class Product {
+
+    render() {    
+        let htmlCatalog = "";
+
+        CATALOG.forEach(({id, name, img, price}) => {
+            htmlCatalog += `
+            <li class="products-element">
+                <span class="products-element__name">${name}</span>
+                <img class="products-element__img" src="${img}"/>
+                <span class="products-element__price">💰 ${price} usd</span>
+                <button class="products-element__btn">Добавить в корзину </button>                
+            </li>
+            `  
+        });
+
+        const htlm = `
+            <ul class="products-container">
+                ${htmlCatalog}
+            </ul>
+        `
+        ROOT_PRODUCT.innerHTML= htlm;
+    } 
+}
+
+const productPage = new Product;
+productPage.render();
---
+++ constant/root.js
@@ -0,0 +1,3 @@
+const ROOT_HEADER = document.getElementById('header');
+const ROOT_SHOP = document.getElementById('shop');
+const ROOT_PRODUCT = document.getElementById('product');
